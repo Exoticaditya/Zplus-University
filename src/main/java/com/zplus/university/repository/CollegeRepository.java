d155b041889f32cd40b8391eac91341acff871ec
Aditya Malik <adityamalik2206950100018@gmail.com>
2026-01-14 14:10:29 +0530

    Fix: Add missing imports in CollegeRepository

--- src/main/java/com/zplus/university/repository/CollegeRepository.java
+++ src/main/java/com/zplus/university/repository/CollegeRepository.java
@@ -2,7 +2,11 @@ package com.zplus.university.repository;
 
 import com.zplus.university.model.College;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CollegeRepository extends JpaRepository<College, Long> {
